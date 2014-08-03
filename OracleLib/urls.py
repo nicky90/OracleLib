e560a038f57bb6dc7c0d9b723194d0e5f1a7e0c5
oraclehcts <wenlin.qiu@oracle.com>
2014-08-03 13:27:38 +0800

    root urls

--- OracleLib/urls.py
+++ OracleLib/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 
+import books
+
 from django.contrib import admin
 admin.autodiscover()
 
@@ -9,4 +11,9 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/register/$', 'books.views.register', name='register'),
+    url(r'^accounts/login/$', 'books.views.login', name='login'),
+    url(r'^accounts/logout/$', 'books.views.logout', name='logout'),
+    url(r'^accounts/profile/(?P<username>\w+)/$', 'books.views.userinfo', name='userinfo'),
+    url(r'^books/', include('books.urls', namespace='books')),
 )
